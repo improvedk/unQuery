a43b68a519d0bd445bccba93de612f6b94769a4f
Mark S. Rasmussen <mark@improve.dk>
2013-11-27 00:39:10 +0100

    Extended type tests for nvarchar and varchar

--- src/unQuery.Tests/SqlTypes/SqlVarcharTests.cs
+++ src/unQuery.Tests/SqlTypes/SqlVarcharTests.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using System.Data;
+using System.Data.SqlClient;
 using unQuery.SqlTypes;
 
 namespace unQuery.Tests.SqlTypes
@@ -12,9 +13,32 @@ namespace unQuery.Tests.SqlTypes
 			var col = (SqlVarchar)"Test";
 			var param = col.GetParameter();
 
+			assertParameter(param, 4, "Test");
+		}
+
+		[Test]
+		public void Constructor()
+		{
+			var col = new SqlVarchar("Test");
+			var param = col.GetParameter();
+
+			assertParameter(param, 4, "Test");
+		}
+
+		[Test]
+		public void ExplicitSize()
+		{
+			var col = new SqlVarchar("Test", 10);
+			var param = col.GetParameter();
+
+			assertParameter(param, 10, "Test");
+		}
+
+		private void assertParameter(SqlParameter param, int size, string value)
+		{
 			Assert.That(param.SqlDbType == SqlDbType.VarChar);
-			Assert.That(param.Size == 4);
-			Assert.That(param.Value.ToString() == "Test");
+			Assert.That(param.Size == size);
+			Assert.That(param.Value.ToString() == value);
 		}
 	}
 }
