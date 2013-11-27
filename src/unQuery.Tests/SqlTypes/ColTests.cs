d022ec988de9534e1b0c8514508c961d8023c1b2
Mark S. Rasmussen <mark@improve.dk>
2013-11-27 22:38:55 +0100

    Added tests for all types to ColTests

--- src/unQuery.Tests/SqlTypes/ColTests.cs
+++ src/unQuery.Tests/SqlTypes/ColTests.cs
@@ -10,27 +10,40 @@ namespace unQuery.Tests.SqlTypes
 		[Test]
 		public void TypeParameters()
 		{
-			// bit
-			TestHelper.AssertParameterFromValue(Col.Bit(false), SqlDbType.Bit, false);
-			TestHelper.AssertParameterFromValue(Col.Bit(null), SqlDbType.Bit, DBNull.Value);
-
 			// bigint
 			TestHelper.AssertParameterFromValue(Col.BigInt(5), SqlDbType.BigInt, 5L);
 			TestHelper.AssertParameterFromValue(Col.BigInt(null), SqlDbType.BigInt, DBNull.Value);
+
+			// bit
+			TestHelper.AssertParameterFromValue(Col.Bit(false), SqlDbType.Bit, false);
+			TestHelper.AssertParameterFromValue(Col.Bit(null), SqlDbType.Bit, DBNull.Value);
 
 			// int
 			TestHelper.AssertParameterFromValue(Col.Int(5), SqlDbType.Int, 5);
 			TestHelper.AssertParameterFromValue(Col.Int(null), SqlDbType.Int, DBNull.Value);
 
-			// varchar
-			TestHelper.AssertParameterFromValue(Col.Varchar("Test"), SqlDbType.VarChar, "Test", 4);
-			TestHelper.AssertParameterFromValue(Col.Varchar("Test", 10), SqlDbType.VarChar, "Test", 10);
-			TestHelper.AssertParameterFromValue(Col.Varchar(null), SqlDbType.VarChar, DBNull.Value);
-
 			// nvarchar
 			TestHelper.AssertParameterFromValue(Col.NVarchar("Test"), SqlDbType.NVarChar, "Test", 4);
 			TestHelper.AssertParameterFromValue(Col.NVarchar("Test", 10), SqlDbType.NVarChar, "Test", 10);
 			TestHelper.AssertParameterFromValue(Col.NVarchar(null), SqlDbType.NVarChar, DBNull.Value);
+
+			// smallint
+			TestHelper.AssertParameterFromValue(Col.SmallInt(5), SqlDbType.SmallInt, (short)5);
+			TestHelper.AssertParameterFromValue(Col.SmallInt(null), SqlDbType.SmallInt, DBNull.Value);
+
+			// tinyint
+			TestHelper.AssertParameterFromValue(Col.TinyInt(5), SqlDbType.TinyInt, (byte)5);
+			TestHelper.AssertParameterFromValue(Col.TinyInt(null), SqlDbType.TinyInt, DBNull.Value);
+
+			// uniqueidentifier
+			var guid = Guid.NewGuid();
+			TestHelper.AssertParameterFromValue(Col.UniqueIdentifier(guid), SqlDbType.UniqueIdentifier, guid);
+			TestHelper.AssertParameterFromValue(Col.UniqueIdentifier(null), SqlDbType.UniqueIdentifier, DBNull.Value);
+
+			// varchar
+			TestHelper.AssertParameterFromValue(Col.Varchar("Test"), SqlDbType.VarChar, "Test", 4);
+			TestHelper.AssertParameterFromValue(Col.Varchar("Test", 10), SqlDbType.VarChar, "Test", 10);
+			TestHelper.AssertParameterFromValue(Col.Varchar(null), SqlDbType.VarChar, DBNull.Value);
 		}
 	}
 }
