662034ff1f7735b42a72020106b0d1238e8af082
Mark S. Rasmussen <mark@improve.dk>
2013-12-03 20:33:30 +0100

    Added test for MapReaderRowsToObject

---
+++ src/unQuery.Tests/MapReaderRowsToObjectTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System.Linq;
+
+namespace unQuery.Tests
+{
+	public class MapReaderRowsToObjectTests : TestFixture
+	{
+		[Test]
+		public void MapReaderRowsToObjectTests_Simple()
+		{
+			var rows = DB.GetRows(@"
+				SELECT 25 AS TestInt, N'abc' AS TestNVarChar
+				UNION ALL
+				SELECT 57 AS TestInt, N'xyz' AS TestNVarChar");
+
+			Assert.AreEqual(2, rows.Count);
+
+			var firstRow = rows.First();
+			Assert.AreEqual(25, firstRow.TestInt);
+			Assert.AreEqual("abc", firstRow.TestNVarChar);
+
+			var secondRow = rows.Skip(1).Single();
+			Assert.AreEqual(57, secondRow.TestInt);
+			Assert.AreEqual("xyz", secondRow.TestNVarChar);
+		}
+	}
+}
