376647d10fee4f5654e4a7b245c226b0621c2d5d
Mark S. Rasmussen <mark@improve.dk>
2013-12-03 23:43:48 +0100

    Converted DynamicFieldMapRow into custom IDynamicMetaObjectProvider for better performance

--- src/unQuery/DynamicFieldMapRow.cs
+++ src/unQuery/DynamicFieldMapRow.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Dynamic;
+using System.Linq.Expressions;
+using System.Reflection;
 
 namespace unQuery
 {
-	public class DynamicFieldMapRow : DynamicObject
+	public class DynamicFieldMapRow : IDynamicMetaObjectProvider
 	{
 		private readonly object[] values;
 		private readonly Dictionary<string, int> fieldMap;
@@ -13,6 +15,18 @@ namespace unQuery
 		{
 			this.values = values;
 			this.fieldMap = fieldMap;
+		}
+
+		public object GetColumnValue(string name)
+		{
+			try
+			{
+				return values[fieldMap[name]];
+			}
+			catch (KeyNotFoundException)
+			{
+				throw new ColumnDoesNotExistException(name);
+			}
 		}
 
 		public static explicit operator Dictionary<string, object>(DynamicFieldMapRow row)
@@ -25,22 +39,44 @@ namespace unQuery
 			return dict;
 		}
 
-		public override bool TryGetMember(GetMemberBinder binder, out object result)
+		DynamicMetaObject IDynamicMetaObjectProvider.GetMetaObject(Expression parameter)
 		{
-			try
-			{
-				result = values[fieldMap[binder.Name]];
-				return true;
-			}
-			catch (KeyNotFoundException)
-			{
-				throw new ColumnDoesNotExistException(binder.Name);
-			}
+			return new DynamicFieldMapRowMetaObject(parameter, this);
+		}
+	}
+
+	internal class DynamicFieldMapRowMetaObject : DynamicMetaObject
+	{
+		private static readonly MethodInfo getColumnValueMethod = typeof(DynamicFieldMapRow).GetMethod("GetColumnValue");
+
+		internal DynamicFieldMapRowMetaObject(Expression parameter, DynamicFieldMapRow value)
+			: base(parameter, BindingRestrictions.Empty, value)
+		{ }
+
+		public override DynamicMetaObject BindGetMember(GetMemberBinder binder)
+		{
+			var parameters = new Expression[] { Expression.Constant(binder.Name) };
+
+			return new DynamicMetaObject(
+				Expression.Call(Expression.Convert(Expression, LimitType), getColumnValueMethod, parameters),
+				BindingRestrictions.GetTypeRestriction(Expression, LimitType)
+			);
 		}
 
-		public override bool TrySetMember(SetMemberBinder binder, object value)
+		/// <summary>
+		/// As we currently do not allow setting values on rows, BindSetMember should never be called.
+		/// </summary>
+		public override DynamicMetaObject BindSetMember(SetMemberBinder binder, DynamicMetaObject value)
 		{
-			throw new InvalidOperationException("Setting values on DynamicFieldMapRow is not supported.");
+			throw new InvalidOperationException();
+		}
+
+		/// <summary>
+		/// As we currently do not allow calling methods on rows, BindInvokeMember should never be called.
+		/// </summary>
+		public override DynamicMetaObject BindInvokeMember(InvokeMemberBinder binder, DynamicMetaObject[] args)
+		{
+			throw new InvalidCastException();
 		}
 	}
 }
