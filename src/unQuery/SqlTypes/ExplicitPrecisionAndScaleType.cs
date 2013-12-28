ab152044e601f1180ca7a6b73f523307b8c8f879
Mark S. Rasmussen <mark@improve.dk>
2013-12-28 23:11:15 +0100

    SqlDecimal now derives from ExplicitPrecisionAndScaleType abstraction

---
+++ src/unQuery/SqlTypes/ExplicitPrecisionAndScaleType.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.SqlServer.Server;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace unQuery.SqlTypes
+{
+	public abstract class ExplicitPrecisionAndScaleType<TValue> : SqlType, ISqlType, ITypeHandler
+	{
+		private readonly TValue value;
+		private readonly byte scale;
+		private readonly byte precision;
+		private readonly bool hasValue;
+		private readonly SqlDbType dbType;
+
+		internal ExplicitPrecisionAndScaleType(SqlDbType dbType)
+		{
+			this.dbType = dbType;
+		}
+
+		internal ExplicitPrecisionAndScaleType(TValue value, byte precision, byte scale, SqlDbType dbType)
+		{
+			this.value = value;
+			this.precision = precision;
+			this.scale = scale;
+			this.dbType = dbType;
+
+			hasValue = true;
+		}
+
+		object ISqlType.GetRawValue()
+		{
+			return value;
+		}
+
+		SqlParameter ISqlType.GetParameter()
+		{
+			return new SqlParameter {
+				SqlDbType = dbType,
+				Value = GetDBNullableValue(value),
+				Precision = precision,
+				Scale = scale
+			};
+		}
+
+		SqlParameter ITypeHandler.CreateParamFromValue(object value)
+		{
+			throw new TypeCannotBeUsedAsAClrTypeException();
+		}
+
+		SqlMetaData ITypeHandler.CreateMetaData(string name)
+		{
+			if (!hasValue)
+				throw new TypeCannotBeUsedAsAClrTypeException();
+
+			return new SqlMetaData(name, dbType, precision, scale);
+		}
+	}
+}
--- src/unQuery/SqlTypes/SqlDecimal.cs
+++ src/unQuery/SqlTypes/SqlDecimal.cs
@@ -1,60 +1,21 @@
-﻿using Microsoft.SqlServer.Server;
-using System.Data;
-using System.Data.SqlClient;
+﻿using System.Data;
 
 namespace unQuery.SqlTypes
 {
-	public class SqlDecimal : SqlType, ISqlType, ITypeHandler
+	public class SqlDecimal : ExplicitPrecisionAndScaleType<decimal?>
 	{
-		private static readonly ITypeHandler typeHandler = new SqlDecimal();
-
-		private readonly decimal? value;
-		private readonly byte precision;
-		private readonly byte scale;
-		private readonly bool hasValue;
-
-		private SqlDecimal()
+		private SqlDecimal() :
+			base(SqlDbType.Decimal)
 		{ }
 
+		public SqlDecimal(decimal? value, byte precision, byte scale) :
+			base(value, precision, scale, SqlDbType.Decimal)
+		{ }
+
+		private static readonly ITypeHandler typeHandler = new SqlDecimal();
 		internal static ITypeHandler GetTypeHandler()
 		{
 			return typeHandler;
-		}
-
-		SqlParameter ITypeHandler.CreateParamFromValue(object value)
-		{
-			throw new TypeCannotBeUsedAsAClrTypeException();
-		}
-
-		SqlMetaData ITypeHandler.CreateMetaData(string name)
-		{
-			if (!hasValue)
-				throw new TypeCannotBeUsedAsAClrTypeException();
-
-			return new SqlMetaData(name, SqlDbType.Decimal, precision, scale);
-		}
-
-		public SqlDecimal(decimal? value, byte precision, byte scale)
-		{
-			this.value = value;
-			this.precision = precision;
-			this.scale = scale;
-			hasValue = true;
-		}
-
-		SqlParameter ISqlType.GetParameter()
-		{
-			return new SqlParameter {
-				SqlDbType = SqlDbType.Decimal,
-				Precision = precision,
-				Scale = scale,
-				Value = GetDBNullableValue(value)
-			};
-		}
-
-		object ISqlType.GetRawValue()
-		{
-			return value;
 		}
 	}
 }
