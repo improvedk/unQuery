a73f09cc04c5024504547e0344d8ee55293eb31a
Mark S. Rasmussen <mark@improve.dk>
2013-12-28 23:07:27 +0100

    SqlDateTimeOffset now dervives from ExplicitScaleType

--- src/unQuery/SqlTypes/SqlDateTimeOffset.cs
+++ src/unQuery/SqlTypes/SqlDateTimeOffset.cs
@@ -1,59 +1,22 @@
-﻿using Microsoft.SqlServer.Server;
-using System;
+﻿using System;
 using System.Data;
-using System.Data.SqlClient;
 
 namespace unQuery.SqlTypes
 {
-	public class SqlDateTimeOffset : SqlType, ISqlType, ITypeHandler
+	public class SqlDateTimeOffset : ExplicitScaleType<DateTimeOffset?>
 	{
-		private static readonly ITypeHandler typeHandler = new SqlDateTimeOffset();
-
-		private readonly DateTimeOffset? value;
-		private readonly byte scale;
-		private readonly bool hasValue;
-
-		private SqlDateTimeOffset()
+		private SqlDateTimeOffset() :
+			base(SqlDbType.DateTimeOffset)
 		{ }
 
+		public SqlDateTimeOffset(DateTimeOffset? value, byte scale) :
+			base(value, scale, SqlDbType.DateTimeOffset)
+		{ }
+
+		private static readonly ITypeHandler typeHandler = new SqlDateTimeOffset();
 		internal static ITypeHandler GetTypeHandler()
 		{
 			return typeHandler;
-		}
-
-		SqlParameter ITypeHandler.CreateParamFromValue(object value)
-		{
-			throw new TypeCannotBeUsedAsAClrTypeException();
-		}
-
-		SqlMetaData ITypeHandler.CreateMetaData(string name)
-		{
-			if (!hasValue)
-				throw new TypeCannotBeUsedAsAClrTypeException();
-
-			return new SqlMetaData(name, SqlDbType.DateTimeOffset, 0, scale);
-		}
-
-		public SqlDateTimeOffset(DateTimeOffset? value, byte scale)
-		{
-			this.value = value;
-			this.scale = scale;
-
-			hasValue = true;
-		}
-
-		SqlParameter ISqlType.GetParameter()
-		{
-			return new SqlParameter {
-				SqlDbType = SqlDbType.DateTimeOffset,
-				Scale = scale,
-				Value = GetDBNullableValue(value)
-			};
-		}
-
-		object ISqlType.GetRawValue()
-		{
-			return value;
 		}
 	}
 }
