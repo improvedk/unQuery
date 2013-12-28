ffbca3867ff336cc55c8fd782f401d4012e21f52
Mark S. Rasmussen <mark@improve.dk>
2013-12-28 23:05:01 +0100

    SqlMoney now derives from ExplicitValueType

--- src/unQuery/SqlTypes/SqlMoney.cs
+++ src/unQuery/SqlTypes/SqlMoney.cs
@@ -1,55 +1,21 @@
-﻿using Microsoft.SqlServer.Server;
-using System.Data;
-using System.Data.SqlClient;
+﻿using System.Data;
 
 namespace unQuery.SqlTypes
 {
-	public class SqlMoney : SqlType, ISqlType, ITypeHandler
+	public class SqlMoney : ExplicitValueType<decimal?>
 	{
-		private static readonly ITypeHandler typeHandler = new SqlMoney();
-
-		private readonly decimal? value;
-		private readonly bool hasValue;
-
-		private SqlMoney()
+		private SqlMoney() :
+			base(SqlDbType.Money)
 		{ }
 
+		public SqlMoney(decimal? value) :
+			base(value, SqlDbType.Money)
+		{ }
+
+		private static readonly ITypeHandler typeHandler = new SqlMoney();
 		internal static ITypeHandler GetTypeHandler()
 		{
 			return typeHandler;
-		}
-
-		SqlParameter ITypeHandler.CreateParamFromValue(object value)
-		{
-			throw new TypeCannotBeUsedAsAClrTypeException();
-		}
-
-		SqlMetaData ITypeHandler.CreateMetaData(string name)
-		{
-			if (!hasValue)
-				throw new TypeCannotBeUsedAsAClrTypeException();
-
-			return new SqlMetaData(name, SqlDbType.Money);
-		}
-
-		public SqlMoney(decimal? value)
-		{
-			this.value = value;
-
-			hasValue = true;
-		}
-
-		SqlParameter ISqlType.GetParameter()
-		{
-			return new SqlParameter {
-				SqlDbType = SqlDbType.Money,
-				Value = GetDBNullableValue(value)
-			};
-		}
-
-		object ISqlType.GetRawValue()
-		{
-			return value;
 		}
 	}
 }
