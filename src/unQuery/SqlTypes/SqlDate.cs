03145db43b0234fb14a2cb3ff1f0cdbcbe3f1ba9
Mark S. Rasmussen <mark@improve.dk>
2013-12-28 23:01:15 +0100

    SqlDate now derives from ExplicitValueType

--- src/unQuery/SqlTypes/SqlDate.cs
+++ src/unQuery/SqlTypes/SqlDate.cs
@@ -1,55 +1,22 @@
-﻿using Microsoft.SqlServer.Server;
-using System;
+﻿using System;
 using System.Data;
-using System.Data.SqlClient;
 
 namespace unQuery.SqlTypes
 {
-	public class SqlDate : SqlType, ISqlType, ITypeHandler
+	public class SqlDate : ExplicitValueType<DateTime?>
 	{
-		private static readonly ITypeHandler typeHandler = new SqlDate();
-
-		private readonly DateTime? value;
-		private readonly bool hasValue;
-
-		private SqlDate()
+		private SqlDate() :
+			base(SqlDbType.Date)
 		{ }
 
+		public SqlDate(DateTime? value) :
+			base(value, SqlDbType.Date)
+		{ }
+
+		private static readonly ITypeHandler typeHandler = new SqlDate();
 		internal static ITypeHandler GetTypeHandler()
 		{
 			return typeHandler;
-		}
-
-		SqlParameter ITypeHandler.CreateParamFromValue(object value)
-		{
-			throw new TypeCannotBeUsedAsAClrTypeException();
-		}
-
-		SqlMetaData ITypeHandler.CreateMetaData(string name)
-		{
-			if (!hasValue)
-				throw new TypeCannotBeUsedAsAClrTypeException();
-
-			return new SqlMetaData(name, SqlDbType.Date);
-		}
-
-		public SqlDate(DateTime? value)
-		{
-			this.value = value;
-			hasValue = true;
-		}
-
-		SqlParameter ISqlType.GetParameter()
-		{
-			return new SqlParameter {
-				SqlDbType = SqlDbType.Date,
-				Value = GetDBNullableValue(value)
-			};
-		}
-
-		object ISqlType.GetRawValue()
-		{
-			return value;
 		}
 	}
 }
