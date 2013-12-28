419d0d26250eea6d793186dd7d2e634fc9d3c45d
Mark S. Rasmussen <mark@improve.dk>
2013-12-28 23:05:52 +0100

    SqlImage now derives from ExplicitMaxLengthType

--- src/unQuery/SqlTypes/SqlImage.cs
+++ src/unQuery/SqlTypes/SqlImage.cs
@@ -1,56 +1,21 @@
-﻿using Microsoft.SqlServer.Server;
-using System.Data;
-using System.Data.SqlClient;
+﻿using System.Data;
 
 namespace unQuery.SqlTypes
 {
-	public class SqlImage : SqlType, ISqlType, ITypeHandler
+	public class SqlImage : ExplicitMaxLengthType<byte[]>
 	{
-		private static readonly ITypeHandler typeHandler = new SqlImage();
-
-		private readonly byte[] value;
-		private readonly bool hasValue;
-
-		private SqlImage()
+		private SqlImage() :
+			base(SqlDbType.Image)
 		{ }
 
+		public SqlImage(byte[] value) :
+			base(value, -1, SqlDbType.Image)
+		{ }
+
+		private static readonly ITypeHandler typeHandler = new SqlImage();
 		internal static ITypeHandler GetTypeHandler()
 		{
 			return typeHandler;
-		}
-
-		SqlParameter ITypeHandler.CreateParamFromValue(object value)
-		{
-			throw new TypeCannotBeUsedAsAClrTypeException();
-		}
-
-		SqlMetaData ITypeHandler.CreateMetaData(string name)
-		{
-			if (!hasValue)
-				throw new TypeCannotBeUsedAsAClrTypeException();
-
-			return new SqlMetaData(name, SqlDbType.Image, -1);
-		}
-
-		public SqlImage(byte[] value)
-		{
-			this.value = value;
-
-			hasValue = true;
-		}
-
-		SqlParameter ISqlType.GetParameter()
-		{
-			return new SqlParameter {
-				SqlDbType = SqlDbType.Image,
-				Value = GetDBNullableValue(value),
-				Size = -1
-			};
-		}
-
-		object ISqlType.GetRawValue()
-		{
-			return value;
 		}
 	}
 }
