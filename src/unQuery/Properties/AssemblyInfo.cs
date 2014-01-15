b7028e9338853022bb3fdb092ed967eb999dae7d
Mark S. Rasmussen <mark@improve.dk>
2014-01-15 21:06:50 +0100

    Released version 0.1.4

--- src/unQuery/Properties/AssemblyInfo.cs
+++ src/unQuery/Properties/AssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Runtime.InteropServices;
 [assembly: InternalsVisibleTo("LINQPadQuery")]
 [assembly: ComVisible(false)]
 [assembly: Guid("6b9551cc-a417-4f83-a658-f896f8ef34b9")]
-[assembly: AssemblyVersion("0.1.3")]
-[assembly: AssemblyFileVersion("0.1.3")]
+[assembly: AssemblyVersion("0.1.4")]
+[assembly: AssemblyFileVersion("0.1.4")]
