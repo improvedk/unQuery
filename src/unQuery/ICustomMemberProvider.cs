df2f80f109456c1f0d5f5ea5f21d91da5d3ad8b2
Mark S. Rasmussen <mark@improve.dk>
2014-05-26 21:24:36 +0200

    Added extra comments to LINQPad interface

--- src/unQuery/ICustomMemberProvider.cs
+++ src/unQuery/ICustomMemberProvider.cs
@@ -4,7 +4,8 @@ using System.Collections.Generic;
 namespace LINQPad
 {
 	/// <summary>
-	/// Special interface implementation that allows LINQPad to visualize DynamicRow values
+	/// Special interface implementation that allows LINQPad to visualize DynamicRow values. LINQPad will pick up on this interface using duck-typing.
+	/// http://www.linqpad.net/faq.aspx
 	/// </summary>
 	public interface ICustomMemberProvider
 	{
