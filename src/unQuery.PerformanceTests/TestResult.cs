69dcec950eb3b1080c7b2e7418ae3c70b56edb94
Mark S. Rasmussen <msr@ipaper.dk>
2014-06-16 21:40:27 +0200

    Added file mistakenly ignored in earlier commit

---
+++ src/unQuery.PerformanceTests/TestResult.cs
@@ -0,0 +1,12 @@
+﻿
+namespace unQuery.PerformanceTests
+{
+	public class TestResult
+	{
+		public int Iterations { get; set; }
+		public double HandcodedAvgRuntimeInMs { get; set; }
+		public double unQueryAvgRuntimeInMs { get; set; }
+		public double unQueryOverheadInPercent { get; set; }
+		public double MaximumUnQueryOverheadInPercent { get; set; }
+	}
+}
